d7a1d7f5d95cbc2e5bd2cc033e5b310bffabf005
Joshua Abbey <joshuaabbey2022@gmail.com>
2022-09-06 18:46:42 +0100

    Reviews added

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 class BookTests(TestCase):
     
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewtest",
+            email="test@email.com",
+            password="testpass123",
+        )
         cls.book = Book.objects.create(
             title = "test title",
             author = "test author",
             price = "10.00",
+        )
+        cls.review = Review.objects.create(
+            book = cls.book,
+            author = cls.user,
+            review = "thats nice",
         )
         
     def test_book_listing(self):
@@ -30,4 +41,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "test title")
+        self.assertContains(response, "thats nice")
         self.assertTemplateUsed(response, "books/book_detail.html")
