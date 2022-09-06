29144c3f490890fd917f1a8beb02cbabecb51e54
Joshua Abbey <joshuaabbey2022@gmail.com>
2022-09-06 08:46:08 +0100

    allauth

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,9 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     # Django Admin
     path('admin/', admin.site.urls),
-    # User Admin
-    path('accounts/', include("django.contrib.auth.urls")),
+    # User Management
+    path('accounts/', include("allauth.urls")),
     # Local Apps
-    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
